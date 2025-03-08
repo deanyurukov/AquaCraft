eb91b7d076751e7c481914de02cc84444f16bfb4
dean10042008 <dean.yurukov2008@gmail.com>
2025-03-08 22:25:42 +0200

    added custom service

--- client/src/assets/configs/email-config.js
+++ client/src/assets/configs/email-config.js
@@ -1,5 +1,6 @@
 export default {
-    emailService: "service_kf8mg1c",
+    gmailService: "service_kf8mg1c",
+    supportService: "service_5ekh4ye",
     registerTemplate: "template_vrj7zyd",
     orderTemplate: "template_v5hm1t6"
 };
