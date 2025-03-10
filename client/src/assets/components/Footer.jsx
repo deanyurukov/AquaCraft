364a1230406f35fc0c8192b02c22a2c41c36edd1
dean10042008 <dean.yurukov2008@gmail.com>
2025-03-10 22:17:51 +0200

    added link to facebook in the footer

--- client/src/assets/components/Footer.jsx
+++ client/src/assets/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <footer id="site-footer">
             <ul className="social-links-container">
-                <li><Link to="#" target="_blank"><i className="fa-brands fa-facebook-f"></i></Link></li>
+                <li><Link to="https://www.facebook.com/profile.php?id=61573735392596" target="_blank"><i className="fa-brands fa-facebook-f"></i></Link></li>
                 <li><Link to="#" target="_blank"><i className="fa-brands fa-twitter"></i></Link></li>
                 <li><Link to="#" target="_blank"><i className="fa-brands fa-linkedin-in"></i></Link></li>
                 <li><Link to="#" target="_blank"><i className="fa-brands fa-instagram"></i></Link></li>
