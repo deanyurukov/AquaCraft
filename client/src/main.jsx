97ddd414e80f249bf2d54be2e2de373d1b35e701
dean10042008 <dean.yurukov2008@gmail.com>
2025-03-27 16:56:47 +0200

    add vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import React, { StrictMode } from 'react';
 import App from './App.jsx';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
     <React.Suspense fallback="Loading...">
