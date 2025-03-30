586e56af23b8db8f82c01b8c06888bfdaa9e3fa9
dean10042008 <dean.yurukov2008@gmail.com>
2025-03-30 11:29:03 +0300

    clicking on contact us on home cards if u dont have an email now alerts u need one

--- client/src/assets/pages/ContactPage.jsx
+++ client/src/assets/pages/ContactPage.jsx
@@ -21,6 +21,7 @@ const ContactPage = () => {
     useEffect(() => {
         if (! isLoggedIn) {
             navigate("/");
+            getErrorAndDisplay("authNeeded");
         }
 
         getUserEmail();
